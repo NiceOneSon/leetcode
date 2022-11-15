3e0735a9243334ff5a9e255077a293a153b9451b
NiceOneSon <103669413+NiceOneSon@users.noreply.github.com>
2022-11-16 02:14:28 +0900

    Time: 224 ms (96.73%), Space: 14 MB (58.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,8 +5,9 @@ class Solution:
         for j in range(length):
             if s[j-l:j+1] == s[j-l:j+1][::-1]:
                 l += 1
-                i = j + 1
             elif j-l-1 >= 0 and s[j-l-1:j+1] == s[j-l-1:j+1][::-1]:
                 l += 2
-                i = j + 1
+            else:
+                continue
+            i = j + 1
         return s[i-l:i]
