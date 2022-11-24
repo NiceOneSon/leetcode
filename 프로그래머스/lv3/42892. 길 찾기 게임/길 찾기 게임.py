f7620a57f7c59da4808375b6e36f67a113de30c1
NiceOneSon <103669413+NiceOneSon@users.noreply.github.com>
2022-11-24 22:27:34 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -0,0 +1,67 @@
+import heapq
+import sys
+sys.setrecursionlimit(10**6)
+
+def solution(nodeinfo):
+    answer = [[]]
+    q = []
+    for ind, (x, y) in enumerate(nodeinfo, start = 1):
+        heapq.heappush(q, (-y, x, ind))
+        
+    class Node:
+        def __init__(self, num, y, x, left, right):
+            self.num = num
+            self.y = y
+            self.x = x
+            self.left = left
+            self.right = right
+    
+        def findnode(self, node, y, x):
+            if x < node.x:
+                left = node.left
+                if left == None:
+                    return node
+                else:
+                    return self.findnode(left, y, x)
+            else:
+                right = node.right
+                if right == None:
+                    return node
+                else:
+                    return self.findnode(right, y, x)
+        
+        def get_nodes(self):
+            if not (self.left or self.right):
+                return [[self.num], [self.num]]
+            forward, backward = [], []
+            forward.extend([self.num])
+            if self.left:
+                num = self.left.get_nodes()
+                forward.extend(num[0])
+                backward.extend(num[1])
+            if self.right:
+                num = self.right.get_nodes()
+                forward.extend(num[0])
+                backward.extend(num[1])
+            backward.extend([self.num])
+            return [forward, backward]
+            
+    prevnode = None
+    root = None
+    while q:
+        y, x, num = heapq.heappop(q)
+        y *= -1
+        node = Node(num, y, x, None, None)
+        if prevnode == None:
+            root = node
+            prevnode = node
+        else:
+            prevnode = root.findnode(root, y, x)            
+            if prevnode.x > x:
+                prevnode.left = node
+            else:
+                prevnode.right = node
+    
+    
+    
+    return root.get_nodes()
