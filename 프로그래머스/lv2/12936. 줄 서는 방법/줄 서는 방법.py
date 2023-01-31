11ee757d1fcae5719115c25d4a1f5a5fcb9ef361
NiceOneSon <103669413+NiceOneSon@users.noreply.github.com>
2023-02-01 01:03:49 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,19 @@
+from math import factorial
+
+
+def solution(n, k):
+    answer = []
+    arr = list(range(1, n+1))
+    ind = 0
+    cnt = 3
+    while k > 1:
+        cnt -= 1
+        for i in range(len(arr)):
+            fact = factorial(len(arr)-1)
+            if fact * i < k:
+                ind = i
+        answer.append(arr[ind])
+        del arr[ind]
+        k -= fact * ind
+    else:
+        return answer + arr
