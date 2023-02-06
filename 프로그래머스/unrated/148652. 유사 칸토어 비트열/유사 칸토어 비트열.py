99eb3a7f3f8b69eadd2dea05642741dba9b8e049
NiceOneSon <103669413+NiceOneSon@users.noreply.github.com>
2023-02-07 00:29:44 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -4,14 +4,14 @@ def solution(n, l, r):
     if l > 1:
         return solution(n, 0, r) - solution(n, 0, l-1)
     
-    if r <= 5 ** (n-1):
+    if r <= (5 ** (n-1)):
         return solution(n-1, 0, r)
-    elif r <= 2 * 5 ** (n-1):
-        return 4 ** (n - 1) + solution(n-1, 0, r - 5 ** (n-1))
-    elif r <= 3 * 5 ** (n-1):
-        return 2 * 4 ** (n - 1)
-    elif r <= 4 * 5 ** (n-1):
-        return 2 * 4 ** (n - 1) + solution(n-1, 0, r - 3 * 5 ** (n-1))
+    elif r <= (2 * 5 ** (n-1)):
+        return 1 * 4 ** (n-1) + solution(n-1, 0, r - 1 * 5**(n-1))
+    elif r <= (3 * 5 ** (n-1)):
+        return 2 * 4 ** (n-1)
+    elif r <= (4 * 5 ** (n-1)):
+        return 2 * 4 ** (n-1) + solution(n-1, 0, r - 3 * 5**(n-1))
     else:
-        return 3 * 4 ** (n - 1) + solution(n-1, 0, r - 4 * 5 ** (n-1))
-    return 0
+        return 3 * 4 ** (n-1) + solution(n-1, 0, r - 4 * 5**(n-1))
+    
