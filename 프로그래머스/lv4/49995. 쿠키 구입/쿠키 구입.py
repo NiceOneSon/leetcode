dc236abc37d1408f581b5ac6ec459797185fd594
NiceOneSon <103669413+NiceOneSon@users.noreply.github.com>
2023-02-09 16:38:00 +0900

    [level 4] Title: 쿠키 구입, Time: 418.31 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv4/49995. 쿠키 구입/쿠키 구입.py
+++ 프로그래머스/lv4/49995. 쿠키 구입/쿠키 구입.py
@@ -3,10 +3,19 @@ from itertools import accumulate
 def solution(cookie):
     answer = 0
     for m in range(len(cookie)-1):
-        a = set(accumulate(reversed(cookie[:m+1])))
-        b = set(accumulate(cookie[m+1:]))
+        a = set()
+        b = set()
+        tmp = 0
+        for ind in range(m, -1, -1):
+            tmp += cookie[ind]
+            a.add(tmp)
+        tmp = 0
+        for ind in range(m+1, len(cookie)):
+            tmp += cookie[ind]
+            b.add(tmp)
+        
         c = a & b
-
+        # print(m, c, f'a : {a}, b : {b}')
         if c:
             answer = max(*c, answer)
     return answer
