67ac6002dd7b8a87ac3b1e37d2b585ed13d20930
NiceOneSon <103669413+NiceOneSon@users.noreply.github.com>
2023-02-12 15:56:09 +0900

    [Gold V] Title: 인구 이동, Time: 4528 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,10 +1,10 @@
 from collections import deque
 
-def bfs(A, n, l, r):
+def bfs(routes, n, l, r):
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
     moved = True
-    cnt = 0
+    cnt = -1
     while moved:
         moved = False
         visited = [[False] * n for _ in range(n)]
@@ -21,27 +21,26 @@ def bfs(A, n, l, r):
                 while q:
                     x, y = q.popleft()
                     union.append((x, y))
-                    s += A[x][y]
+                    s += routes[x][y]
                     for k in range(4):
                         nx, ny = x + dx[k], y + dy[k]
                         if nx < 0 or nx >= n or ny < 0 or ny >= n:
                             continue
                         if visited[nx][ny]:
                             continue
-                        diff = abs(A[nx][ny] - A[x][y])
+                        diff = abs(routes[nx][ny] - routes[x][y])
                         if diff >= l and diff <= r:
                             q.append((nx, ny))
                             visited[nx][ny] = True
                             moved = True
                 avg = s // len(union)
                 for x, y in union:
-                    A[x][y] = avg
-        if moved:
-            cnt += 1
+                    routes[x][y] = avg
+        cnt += 1
     return cnt
 
 n, l, r = map(int, input().split())
-A = []
+routes= []
 for i in range(n):
-    A.append(list(map(int, input().split())))
-print(bfs(A, n, l, r))
+    routes.append(list(map(int, input().split())))
+print(bfs(routes, n, l, r))
