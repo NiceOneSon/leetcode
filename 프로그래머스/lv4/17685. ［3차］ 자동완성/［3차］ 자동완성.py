c9ec7c15f5960739798d19bcc9c2bbf3efa2e0b9
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-04-18 13:03:21 +0900

    [level 4] Title: [3차] 자동완성, Time: 1704.93 ms, Memory: 398 MB -BaekjoonHub

--- 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
+++ 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.py
@@ -1,28 +1,47 @@
-from collections import defaultdict
-
-def recur(words, left, right, depth):
-    
-    if left + 1 == right:
-        # print('!!finish', left, right, depth, words[left][:depth+1])
-        return depth
-    answer = 0
-    prev = words[left][:depth+1]
-    start = left
-    for ind in range(left+1, right):
-        if prev == words[ind][:depth+1]:
-            # print(f'#continue  prev : {prev}, ind : {ind}, word : {words[ind][:depth+1]}')
-            continue
-        else:
-            # print(f'#recur  prev : {prev}, ind : {ind}, word : {words[ind][:depth+1]}')
-            answer += recur(words, start, ind, min(len(prev), depth+1))
-            start = ind
-            prev = words[ind][:depth+1]
-    else:
-        answer += recur(words, start, right, depth+1)
-    return answer
+class Node:
+    def __init__(self, word = ''):
+        self.word = word
+        self.cnt = 1
+        self.childnode = {}
         
-    
+class Trie:
+    def __init__(self):
+        self.root = Node()
+        
+        
+    def insert(self, word):
+        node = self.root
+        node.cnt += 1
+        control = True
+        for char in word:
+            if char in node.childnode:
+                node.childnode[char].cnt += 1
+                node = node.childnode[char]
+                
+            else:
+                newnode = Node(char)
+                node.childnode[char] = newnode
+                node = newnode
+                
+    def find(self, word):
+        node = self.root
+        result = 0
+        for char in word:
+            if char in node.childnode and node.childnode[char].cnt > 1:
+                node = node.childnode[char]
+                result += 1
+            else:
+                result += 1
+                break
+        
+        return result
+                
+
 def solution(words):
     answer = 0
-    words.sort()
-    return recur(words, 0, len(words), 0)
+    trie = Trie()
+    for word in words:
+        trie.insert(word)
+    for word in words:
+        answer += trie.find(word)
+    return answer
