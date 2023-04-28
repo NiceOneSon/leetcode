47c268fe920402ec4243a5a881d92d5383bc92ca
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-04-28 21:35:08 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 31.97 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,25 +1,42 @@
-from functools import reduce
+# DP
+# TC : 150 * 150 * 100
+
 def solution(alp, cop, problems):
-    # alp, cop을 기준으로 2차원 배열 생성 # row = alp, col = cop
-    alpmax, copmax = reduce(lambda x, y : max(x, y[0]), problems, 0), reduce(lambda x, y : max(x, y[1]), problems, 0)
+    # init
     INF = float('inf')
-    routes = [[INF] * (copmax+1) for i in range(alpmax+1)]
-    alp = min(alp, alpmax)
-    cop = min(cop, copmax)
-    # 한 시간씩 공부했을 때 (최악의 경우) 투자해야 하는 시간 세팅
-    routes[alp][cop] = 0
+    answer = INF
+    row, col = 151, 151
+    DP = [[INF] * col for _ in range(row)]
     
-    # Problems하나씩 빼서 공부했을 때를 업데이트 둘 다 최대 크기까지(만일 세팅된 값보다 크다면 break)
-    for r in range(alp, alpmax+1):
-        for c in range(cop, copmax+1):
-            if r + 1 <= alpmax:
-                routes[r+1][c] = min(routes[r+1][c], routes[r][c] + 1)
-            if c + 1 <= copmax:
-                routes[r][c+1] = min(routes[r][c+1], routes[r][c] + 1)
-            for abil_alp, abil_cop, exp_r, exp_c, cost in problems:
-                if r >= abil_alp and c >= abil_cop:
-                    newr = (r + exp_r) if r + exp_r < alpmax else alpmax
-                    newc = (c + exp_c) if c + exp_c < copmax else copmax
-                    routes[newr][newc] = min(routes[newr][newc], routes[r][c] + cost)
-    answer = routes[-1][-1]
+    for y in range(row):
+        for x in range(col):
+            if y < alp and x < cop:
+                DP[y][x] = 0
+            elif y < alp and x >= cop:
+                DP[y][x] = x - cop
+            elif y >= alp and x < cop:
+                DP[y][x] = y - alp
+            else:
+                DP[y][x] = y - alp +  x - cop
+    
+    maxal, maxco = 0, 0
+    for al, co, _, _, _ in problems:
+        maxal = max(maxal, al)
+        maxco = max(maxco, co)
+    
+    for y in range(alp, row):
+        for x in range(cop, col):
+            for lalp, lcop, add_al, add_co, cost in problems:
+                if y >= lalp and x >=lcop:
+                    sy, sx = y+add_al, x+add_co
+                    sy, sx = min(sy, row-1), min(sx, col-1)
+                    DP[sy][sx] = min(DP[sy][sx], DP[y][x] + cost)
+    
+    for y in range(row):
+        for x in range(col):
+            if y >= maxal and x >= maxco:
+                if answer > DP[y][x]:
+                    answer = DP[y][x]
+    
     return answer
+            
