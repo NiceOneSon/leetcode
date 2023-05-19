4aa9e666cf5cfa40eeb27f47717503cc0157c01d
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-05-20 00:02:38 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 264 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,20 +1,18 @@
 N = int(input())
-routes = list(map(int, input().split()))
+
+routes = list(map(int, input().split(' ')))
+
+DP = [1] * (len(routes)+1)
+for i in range(1, len(routes)):
+    for j in range(i):
+        if routes[i] > routes[j]:
+            DP[i] = max(DP[j]+1, DP[i])
+DP_rev = [1] * (len(routes) + 1)
+for i in range(len(routes)-2, -1, -1):
+    for j in range(len(routes)-1, i, -1):
+        if routes[i] > routes[j]:
+            DP_rev[i] = max(DP_rev[j]+1, DP_rev[i])
 answer = 0 
-DP = [1] * N
-reversed_DP = [1] * N
-for i in range(1, N):
-    for j in range(i):
-        if routes[i] > routes[j]:
-            DP[i] = max(DP[i], DP[j]+1)
-
-routes = routes[::-1]
-
-for i in range(1, N):
-    for j in range(i):
-        if routes[i] > routes[j]:
-            reversed_DP[i] = max(reversed_DP[i], reversed_DP[j]+1)
-
-for i in range(N):
-    answer = max(answer, DP[i]+reversed_DP[N-1-i])
-print(answer-1)
+for i in range(len(routes)):
+    answer = max(answer, DP[i] + DP_rev[i] - 1)
+print(answer)
