a892d846d7a2e8d8c1234cc8acd40db869885987
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-06-22 00:13:27 +0900

    [unrated] Title: 연속 펄스 부분 수열의 합, Time: 426.27 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/README.md
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [unrated] 연속 펄스 부분 수열의 합 - 161988 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=python3) 
 
 ### 성능 요약
 
-메모리: 59.7 MB, 시간: 343.24 ms
+메모리: 73.4 MB, 시간: 426.27 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,9 +1,31 @@
+class funcs:
+    def getmaxnum(sequence):
+        answer = 0
+        result = 0
+        left = 0
+        for right in range(len(sequence)):
+            num = sequence[right]
+            result += num
+            while result < 0 and left <= right:
+                result -= sequence[left]
+                left += 1
+            answer = max(answer, result)
+        return answer 
+    
 def solution(sequence):
-    DP1 = [0] * len(sequence)
-    DP2 = [0] * len(sequence)
+    answer = 0
+    # 두 가지 펄스를 곱한 결과 리스트 SeqFirstPlusOne, SeqFirstMinusOne
+    # 투 포인터로 문제 해결
+    SeqFirstPlusOne, SeqFirstMinusOne = [], []
+    boolean = True
     for i in range(len(sequence)):
-        boolean = i%2
-        alpha = 1 if boolean else -1
-        DP1[i] = max(DP1[i], DP1[i-1] + sequence[i]*alpha)
-        DP2[i] = max(DP2[i], DP2[i-1] + sequence[i]*(-1*alpha))
-    return max(max(DP1), max(DP2))
+        num = sequence[i]
+        SeqFirstPlusOne.append(num * (1 if boolean else -1))
+        SeqFirstMinusOne.append(num * (1 if not boolean else -1))
+        boolean = not boolean
+    
+    result1 = funcs.getmaxnum(SeqFirstPlusOne)
+    result2 = funcs.getmaxnum(SeqFirstMinusOne)
+    # print(result2, SeqFirstMinusOne)
+    
+    return max(result1, result2)
