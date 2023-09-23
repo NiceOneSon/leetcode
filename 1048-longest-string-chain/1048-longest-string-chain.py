581b947a0dc2bef50a019f54edec377c42c88de6
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-09-23 18:01:55 +0900

    Time: 224 ms (42.03%), Space: 20.4 MB (5.78%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,33 +1,35 @@
-from functools import lru_cache
+# from collections import defaultdict
 
 class Solution:
-    
-    s = None
-    @lru_cache
-    def recursive(self, word, cnt):
-        s = Solution.s
-        answer = 0
-        for i in range(len(word)):
-            newword = word[:i] + word[i+1:]
-            if newword in s:
-                answer = max(answer, self.recursive(newword, cnt + 1))
-        if answer:
-            return answer
-        else:
-            return cnt + 1
-        
-
-    
     def longestStrChain(self, words: List[str]) -> int:
+        subset = set(words)
+        g = defaultdict(list)
+
+        for word in words:
+            for j in range(len(word)):
+                substring = word[:j] + word[j+1:]
+                g[substring].append(word)
         
-        s = set(words)
-        Solution.s = s
-        words.sort(key = lambda x : len(x))
         
+        
+        def bfs(word):
+            Q = deque()
+            Q.append((word, 1))
+            visited[word] = True
+            maxcnt = 0
+            while Q:
+                word, cnt = Q.popleft()
+                maxcnt = max(maxcnt, cnt)
+                for nei in g[word]:
+                    if visited[nei]:
+                        continue
+                    visited[nei] = True
+                    Q.append((nei, cnt + 1))
+            return maxcnt
+
         answer = 0
-        
-        while words:
-            word = words.pop()
-            answer = max(answer, self.recursive(word, 0))
-            
+        for word in subset:
+            visited = defaultdict(int)
+            depth = bfs(word)
+            answer = max(answer, depth)
         return answer
