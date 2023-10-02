4a664ffcaf918f696c0d330ee957793e7ef77ede
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-10-02 22:50:36 +0900

    Time: 177 ms (85.48%), Space: 17.4 MB (65.73%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -5,9 +5,10 @@ class Solution:
         A = 0
         B = 0
         for i in range(2, len(colors)):
-            if colors[i-2:i+1] == 'AAA':
-                A += 1
-            elif colors[i-2:i+1] == 'BBB':
-                B += 1
+            if colors[i] == colors[i-1] == colors[i-2]:
+                if colors[i] == 'A':
+                    A += 1
+                else:
+                    B += 1
         
         return A > B
