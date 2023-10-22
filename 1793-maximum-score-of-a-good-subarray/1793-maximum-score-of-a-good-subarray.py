3a58dde85f5ddb6f5368be58a9ae369c3c7067d5
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-10-22 13:51:39 +0900

    Time: 1345 ms (5.10%), Space: 27.4 MB (46.94%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        left, right = k, k
+        result = nums[k]
+        minNum = nums[k]
+        answer = nums[k]
+        def move(isLeft, result, minNum, left, right):
+            if isLeft:
+                return min(minNum, nums[left - 1]) * (right + 2 - left), min(minNum, nums[left - 1])
+            else:
+                return min(minNum, nums[right + 1]) * (right + 2 - left), min(minNum, nums[right + 1])
+            
+        while left > 0 and right < len(nums) - 1:
+            goLeft, leftMinNum = move(True, result, minNum, left, right)
+            goRight, rightMinNum = move(False, result, minNum, left, right)
+            if goRight > goLeft:
+                answer = max(answer, goRight)
+                minNum = rightMinNum
+                right += 1
+                # print(left, right, answer)
+            else:
+                answer = max(answer, goLeft)
+                minNum = leftMinNum
+                left -= 1
+                # print(left, right, answer)
+        else:
+            if left == 0:
+                while right < len(nums) - 1:
+                    goRight, rightMinNum = move(False, result, minNum, left, right)
+                    answer = max(answer, goRight)
+                    minNum = rightMinNum
+                    right += 1
+                    # print(left, right, answer)
+            else:
+                while left >= 0:
+                    goLeft, leftMinNum = move(True, result, minNum, left, right)
+                    answer = max(answer, goLeft)
+                    minNum = leftMinNum
+                    left -= 1
+                    # print(left, right, answer)
+        return answer
+            
