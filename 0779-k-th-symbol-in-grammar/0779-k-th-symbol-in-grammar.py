8a70ba607ad34f2d56213b90a7c2d0e4b160dda7
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-10-26 00:29:13 +0900

    Time: 40 ms (46.39%), Space: 16.1 MB (97.42%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,6 +1,6 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1 and k <= 1:
+        if n == 1 and k == 1:
             return 0
         result = self.kthGrammar(n - 1, (k - 1)//2 + 1)
         isLeft = (k % 2) != 0
