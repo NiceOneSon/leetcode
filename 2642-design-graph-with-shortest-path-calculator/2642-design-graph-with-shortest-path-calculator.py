b7d23db14cccaacb80076c345b79e24a028d3297
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-11-11 16:14:53 +0900

    Time: 2557 ms (12.36%), Space: 19.9 MB (10.00%) - LeetHub

---
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+import heapq
+
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]) -> None:
+        self.graph = defaultdict(lambda : defaultdict(lambda : float('inf')))
+        for f, t, v in edges:
+            self.graph[f][t] = v
+        return None
+        
+
+    def addEdge(self, edge: List[int]) -> None:
+        f, t, v = edge
+        self.graph[f][t] = v
+        return None
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        if node1 == node2:
+            return 0
+        q = []
+        for other in self.graph[node1].keys():
+            heapq.heappush(q, (self.graph[node1][other], other))
+        while q:
+            dist, node = heapq.heappop(q)
+            for other in self.graph[node].keys():
+                if self.graph[node1][other] > self.graph[node][other] + dist:
+                    heapq.heappush(q, (self.graph[node][other] + dist, other))
+                    self.graph[node1][other] = self.graph[node][other] + dist
+                    
+        if self.graph[node1][node2] == float('inf'):
+            return -1
+        return self.graph[node1][node2]
+
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
