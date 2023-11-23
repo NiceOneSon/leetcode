5e08a2456bbb1db242133d2350e4e36835e23a51
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-11-23 12:45:47 +0900

    Time: 226 ms (13.81%), Space: 16.5 MB (39.64%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -1,4 +1,25 @@
+import heapq
+
 class Solution:
+
+    def fillOutQueue(q : list) -> bool:
+        left, right = heapq.heappop(q), heapq.heappop(q)
+        diff = right - left
+
+        while q:
+            left, right = left, right = right, heapq.heappop(q)
+            if right - left != diff:
+                return False
+        return True
+        
+        
+    def fillUpQueue(fromIdx : int, toIdx : int, nums : List[int]) -> List[int]:
+        q = []
+        for numIdx in range(fromIdx, toIdx+1):
+            heapq.heappush(q, nums[numIdx])
+        return q
+    
+    
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
         import heapq
         
@@ -8,23 +29,14 @@ class Solution:
 
             fromIdx, toIdx = l[idx], r[idx]
             
-            q = []
-            
-            for numIdx in range(fromIdx, toIdx+1):
-                heapq.heappush(q, nums[numIdx])
+            q = Solution.fillUpQueue(fromIdx, toIdx, nums)
             
             if len(q) == 1:
                 answer.append(True)
+                continue
             
-            left, right = heapq.heappop(q), heapq.heappop(q)
-            diff = right - left
-            while q:
-                left, right = right, heapq.heappop(q)
-                if right - left != diff:
-                    answer.append(False)
-                    break
-            else:
-                answer.append(True)
+            isArithmetic = Solution.fillOutQueue(q)
+            answer.append(isArithmetic)
         
         return answer
             
