b915e9c95582ead85ddd82ae27706e10e6a65b43
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-12-13 13:01:31 +0900

    Time: 155 ms (58.10%), Space: 16.8 MB (33.33%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -1,30 +1,19 @@
 class Solution:
-    
-    def checkIsSpecial(self, y: int, x: int, mat: List[List[int]]) -> int:
-        answer = True
-        cnt = 0
-        for xtmp in range(len(mat[0])):
-            if mat[y][xtmp]:
-                cnt += 1
-        
-        for ytmp in range(len(mat)):
-            if mat[ytmp][x]:
-                cnt += 1
-        
-        if cnt == 2:
-            return 1
-        return 0
-            
-    
     def numSpecial(self, mat: List[List[int]]) -> int:
         ylen, xlen = len(mat), len(mat[0])
-        
         answer = 0
+        col = [0] * xlen
+        row = [0] * ylen
         
         for y in range(ylen):
             for x in range(xlen):
                 if mat[y][x]:
-                    answer += self.checkIsSpecial(y, x, mat)
-                    
+                    col[x] += 1
+                    row[y] += 1
+        
+        for y in range(ylen):
+            for x in range(xlen):
+                if mat[y][x] and col[x] == 1 and row[y] == 1:
+                    answer += 1
         
         return answer
