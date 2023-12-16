6f0a071dea2ee6ea3e0ef7122af369367a8d1d02
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-12-16 12:56:40 +0900

    Time: 63 ms (15.78%), Space: 17.6 MB (9.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        s = list(s)
+        t = list(t)
         
-        from collections import defaultdict
+        s.sort()
+        t.sort()
         
-        word_dict = defaultdict(int)
-        
-        for source_string in s:
-            word_dict[source_string] += 1
-        
-        for target_string in t:
-            word_dict[target_string] -= 1
-            if word_dict[target_string] < 0:
-                return False
-        
-        return True
-        
+        if s == t:
+            return True
+        return False
