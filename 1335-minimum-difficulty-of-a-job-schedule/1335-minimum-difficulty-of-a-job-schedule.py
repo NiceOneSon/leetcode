bbc1784f9337bc65fcf26a62de514ea81059d992
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2023-12-29 21:31:53 +0900

    Time: 3424 ms (5.08%), Space: 243.3 MB (5.09%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], remain: int) -> int:
+        INF = float('inf')
+        
+        if remain > len(jobDifficulty):
+            return -1
+        
+        DP = [[0] * (len(jobDifficulty)) for _ in range(len(jobDifficulty))]
+        for i in range(len(jobDifficulty)):
+            maxval = jobDifficulty[i]
+            DP[i][i] = maxval
+            for j in range(i, len(jobDifficulty)):
+                maxval = max(maxval, jobDifficulty[j])
+                DP[i][j] = maxval
+            
+        # print(DP)
+        @lru_cache(None)
+        def recursive(left: int, right: int, d: int):
+            if d == 0:
+                if right == len(jobDifficulty):
+                    if right == left:
+                        return 0
+                    return DP[left][right-1]
+                else:
+                    return INF
+            
+            if right >= len(jobDifficulty):
+                return INF
+            
+            answer = INF
+            answer = min(answer, recursive(left, right + 1, d))
+            answer = min(answer, recursive(right+1, right+1, d - 1) + DP[left][right])
+            
+            return answer
+        return recursive(0, 0, remain)
