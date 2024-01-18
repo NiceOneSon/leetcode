b98c99208c7fee9466ccde74b78af4c53d8c32cd
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-01-18 11:52:23 +0900

    Time: 39 ms (43.82%), Space: 17.2 MB (31.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,11 +2,7 @@ from functools import lru_cache
 class Solution:
     @lru_cache(50)
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        if n == 3:
-            return 3
+        if n <= 3:
+            return n
         
         return self.climbStairs(n-1) + self.climbStairs(n-2) 
