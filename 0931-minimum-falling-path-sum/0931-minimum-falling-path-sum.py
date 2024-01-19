aee85e13167f521d3218794c40e2d143dbe3d3f5
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-01-19 12:50:31 +0900

    Time: 141 ms (45.09%), Space: 18.2 MB (40.85%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        y, x = len(matrix), len(matrix[0])
+        
+        answer = [[float('inf')] * x for _ in range(y)]
+        
+        min_idx = 0
+        row = matrix[0]
+        for i in range(x):
+            answer[0][i] = row[i]
+        
+        
+        
+        dy, dx = (-1, -1, -1), (-1, 0, 1)
+        print(answer)
+        for yi in range(1, y):
+            for xi in range(x):
+                for i in range(3):
+                    sy, sx = yi + dy[i], xi + dx[i]
+                    
+                    if not (0<=sx<x):
+                        continue
+                    # print(answer[yi][xi], matrix[yi], yi, xi, sy, sx)
+                    answer[yi][xi] = min(answer[yi][xi], matrix[yi][xi] + answer[sy][sx])
+        
+        return min(answer[-1])
+        
