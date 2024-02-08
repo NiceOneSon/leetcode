a7fdb03dd758c377b9de97688e9c5eb94bd092bf
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-02-08 12:39:21 +0900

    Time: 4296 ms (15.09%), Space: 19.1 MB (24.07%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,17 +1,13 @@
 from functools import lru_cache 
 
 class Solution:
-    limit = 100    
+    
     @lru_cache(10**4)
     def numSquares(self, n: int) -> int:
         if n < 0:
             return float('inf')
         if n == 0:
             return 0
-        # print(n)
-        # Solution.limit -= 1
-        # if Solution.limit == 0:
-        #     return 0
         num = int(n ** 0.5)
         answer = float('inf')
         while num > 0:
