2ba366b875b155535e8cb8c968d14c8691938dbc
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-03-11 20:55:44 +0900

    Time: 33 ms (73.85%), Space: 16.5 MB (61.09%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -5,7 +5,7 @@ class Solution:
         answer = []
         for alphabet in order:
             answer += [alphabet] * c[alphabet]
-        
+        order = set(order)
         for alphabet in s:
             if alphabet not in order:
                 answer.append(alphabet)
