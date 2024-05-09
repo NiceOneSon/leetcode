be21bd68573998bcd5ae60b17b54d7b9e3ba92ec
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-05-09 16:27:47 +0900

    Time: 784 ms (84.65%), Space: 42.7 MB (90.19%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -1,15 +1,9 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        import heapq
-        
-        q = []
-        
         happiness.sort()
-        
         answer = 0
         for count in range(k):
             person = happiness.pop()
             if person - count > 0:
                 answer += person - count
-        
         return answer
