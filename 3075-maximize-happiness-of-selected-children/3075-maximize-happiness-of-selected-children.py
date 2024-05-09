227c58aaf6f7d833d438761ee6ea17df5ea9ff36
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-05-09 16:04:44 +0900

    Time: 783 ms (85.86%), Space: 42.4 MB (95.09%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -4,13 +4,11 @@ class Solution:
         
         q = []
         
-        for person in happiness:
-            heapq.heappush(q, -person)
+        happiness.sort()
         
         answer = 0
         for count in range(k):
-            minus_person = heapq.heappop(q)
-            person = -minus_person
+            person = happiness.pop()
             if person - count > 0:
                 answer += person - count
         
