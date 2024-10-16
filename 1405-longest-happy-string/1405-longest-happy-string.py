d75692b4e8642f021a42a0ae172aa8d3792c7799
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-10-16 13:14:18 +0900

    Time: 34 ms (69.04%), Space: 16.8 MB (29.54%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -4,32 +4,21 @@ class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         q = []
         s = []
-        heapq.heappush(q, (-a, 1))
-        heapq.heappush(q, (-b, 2))
-        heapq.heappush(q, (-c, 3))
+        heapq.heappush(q, (-a, 'a'))
+        heapq.heappush(q, (-b, 'b'))
+        heapq.heappush(q, (-c, 'c'))
         
         while q:
-            n, pnt = heapq.heappop(q)
-            
-            if len(s) >= 2 and pnt == s[-1] and pnt == s[-2]:
-                if q:
-                    _n, _pnt = heapq.heappop(q)
-                    heapq.heappush(q, (n, pnt))
-                    n, pnt = _n, _pnt
+            n, alphabet = heapq.heappop(q)
+            if len(s) >= 2 and alphabet == s[-1] and alphabet == s[-2]:
                 if not q:
                     continue
+                _n, _alphabet = heapq.heappop(q)
+                heapq.heappush(q, (n, alphabet))
+                n, alphabet = _n, _alphabet
             if n >= 0:
                 continue
-            s.append(pnt)
-            heapq.heappush(q, (n+1, pnt))
-            
-            
-        for idx in range(len(s)):
-            if s[idx] == 1:
-                s[idx] = 'a'
-            elif s[idx] == 2:
-                s[idx] = 'b'
-            else:
-                s[idx] = 'c'
+            s.append(alphabet)
+            heapq.heappush(q, (n+1, alphabet))
 
         return ''.join(s)
