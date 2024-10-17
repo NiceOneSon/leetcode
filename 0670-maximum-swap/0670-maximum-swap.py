4afa9b1b702e774915336f23517678535d418665
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-10-17 18:43:03 +0900

    Time: 44 ms (5.28%), Space: 16.8 MB (10.80%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,4 +1,21 @@
 class Solution:
+    def swap(self, q, pnts):
+        for idx in range(len(q)-1, -1, -1):
+            pnt = pnts[idx]
+            if q[idx] < q[pnt]:
+                q[idx], q[pnt] = q[pnt], q[idx]
+                break
+    
+    def value_return(self, q, pnts):
+        answer = 0
+        for pnt in range(len(q)):
+            answer += q[pnt] * 10 ** pnt
+        return answer
+    
+    def swap_and_return(self, q, pnts):
+        self.swap(q = q, pnts = pnts)    
+        return self.value_return(q, pnts)
+        
     def maximumSwap(self, num: int) -> int:
         q = []
         pnts = []
@@ -14,13 +31,7 @@ class Solution:
             q.append(n)
             pnts.append(pnt)
         
-        for idx in range(len(q)-1, -1, -1):
-            pnt = pnts[idx]
-            if q[idx] < q[pnt]:
-                q[idx], q[pnt] = q[pnt], q[idx]
-                break
-                
-        answer = 0
-        for pnt in range(len(q)):
-            answer += q[pnt] * 10 ** pnt
-        return answer
+        return self.swap_and_return(q, pnts)
+        
+        
+        
