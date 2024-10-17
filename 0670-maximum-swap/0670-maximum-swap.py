887944b848cb513125d56006dd1416d50a245e26
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-10-17 17:56:09 +0900

    Time: 42 ms (8.29%), Space: 16.6 MB (42.30%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -8,14 +8,11 @@ class Solution:
         
         for curr_idx in range(length-2, -1, -1):
             right_idx = curr_idx + 1
-            left_pnt = curr_idx
             right_pnt = dp[right_idx]
-            
-            pnt = curr_idx
-            if lnum[left_pnt] <= lnum[right_pnt]:
-                pnt = right_pnt
-                
-            dp[curr_idx] = pnt
+            curr_pnt = curr_idx
+            if lnum[curr_pnt] <= lnum[right_pnt]:
+                curr_pnt = right_pnt
+            dp[curr_idx] = curr_pnt
         
         for curr_idx in range(length):
             pnt = dp[curr_idx]
