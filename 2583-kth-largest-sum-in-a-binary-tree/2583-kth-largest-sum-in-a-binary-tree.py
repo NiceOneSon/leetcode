d8d7fb99155ebe73c3aa93aeec7300b82480cdcf
Jinyoung Son <103669413+NiceOneSon@users.noreply.github.com>
2024-10-22 12:24:01 +0900

    Time: 87 ms (97.81%), Space: 66.1 MB (5.02%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.py
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.py
@@ -1,30 +1,28 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
         from collections import defaultdict
-        
+
         d = defaultdict(int)
-        
+            
         def search(curr: TreeNode, height = 0):
             if not curr:
                 return
-            
+
             left = curr.left
             right = curr.right
-            
+
             d[height] += curr.val
-            
+
             search(left, height + 1)
             search(right, height + 1)
+            
+        def find_kth_sum(k: int):
+            q = [val for val in d.values()]
+            q.sort(reverse = True)
+            if k > len(q):
+                return -1
+            return q[k-1]
         
-        search(root)
-    
-        q  = [(val, key) for key, val in d.items()]
-        q.sort(reverse = True)
-        if k > len(q): return -1
-        return q[k-1][0]
+        search(curr = root, height = 0)
+
+        return find_kth_sum(k)
